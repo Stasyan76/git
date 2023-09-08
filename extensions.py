271e953cc22132d224c472e7d7b07a5f57724586
Stas <Stasyan5757@mail.ru>
2023-09-08 11:03:17 +0700

    Бот для курса валют

--- extensions.py
+++ extensions.py
@@ -8,16 +8,16 @@ class APIException(Exception):
 class StaticValue:
     @staticmethod
     def get_price(quote: str, base: str, amount: str):
-        if base not in all_values:
+        if quote not in all_values and base not in all_values and not amount.isdigit():
+            raise APIException(f'Кодов валюты {base} и {quote} не существует, а также {amount} не является числом!')
+        elif quote not in all_values and base not in all_values:
+            raise APIException(f'Кодов валюты {base} и {quote} не существует')
+        elif base not in all_values:
             raise APIException(f'Кода валюты {base} не существует')
         elif quote not in all_values:
             raise APIException(f'Кода валюты {quote} не существует')
-        elif quote not in all_values and base not in all_values:
-            raise APIException(f'Кода валюты {base} и {quote} не существует')
         elif not amount.isdigit():
             raise APIException(f'{amount} не является числом!')
-
         text = r.get(f'https://min-api.cryptocompare.com/data/price?fsym={base}&tsyms={quote}')
         result = json.loads(text.content)
-
         return result
